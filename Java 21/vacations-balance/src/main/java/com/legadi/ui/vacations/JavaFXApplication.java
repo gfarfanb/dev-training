722421ff254ed21dfd695c25499f49208de29852
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-11-26 18:38:57 -0600

    Spring boot integration

---
+++ Java 21/vacations-balance/src/main/java/com/legadi/ui/vacations/JavaFXApplication.java
@@ -0,0 +1,72 @@
+package com.legadi.ui.vacations;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class JavaFXApplication extends Application {
+
+    private ConfigurableApplicationContext context;
+
+    @Override
+    public void init() throws Exception {
+        ApplicationContextInitializer<GenericApplicationContext> initializer = new ApplicationContextInitializer<GenericApplicationContext>() {
+            @Override
+            public void initialize(GenericApplicationContext genericApplicationContext) {
+                genericApplicationContext.registerBean(Application.class, () -> JavaFXApplication.this);
+                genericApplicationContext.registerBean(Parameters.class, () -> getParameters());
+                genericApplicationContext.registerBean(HostServices.class, () -> getHostServices());
+            }
+        };
+        this.context = new SpringApplicationBuilder().sources(VacationsBalanceApplication.class)
+            .initializers(initializer)
+            .build().run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        if(this.context != null) {
+            this.context.publishEvent(new StageReadyEvent(primaryStage));
+        }
+
+        FXMLLoader loader = new FXMLLoader(Thread
+            .currentThread()
+            .getContextClassLoader()
+            .getResource("vacations-ui.fxml"));
+        VBox page = (VBox) loader.load();
+        Scene scene = new Scene(page);
+
+        primaryStage.setTitle("Balance de vacaciones");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if(this.context != null) {
+            this.context.close();
+        }
+        Platform.exit();
+    }
+
+    class StageReadyEvent extends ApplicationEvent {
+
+        public Stage getStage() {
+            return Stage.class.cast(getSource());
+        }
+
+        public StageReadyEvent(Object source) {
+            super(source);
+        }
+    }
+}
--- Java 21/vacations-balance/src/main/java/com/legadi/ui/vacations/VacationsBalanceApplication.java
+++ Java 21/vacations-balance/src/main/java/com/legadi/ui/vacations/VacationsBalanceApplication.java
@@ -1,39 +1,13 @@
 package com.legadi.ui.vacations;
 
-import java.net.URLClassLoader;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-// @SpringBootApplication
-public class VacationsBalanceApplication extends Application {
-
-    // @Override
-    // public void init() throws Exception {
-    //     SpringApplicationBuilder builder = new SpringApplicationBuilder(VacationsBalanceApplication.class);
-    //     builder.application().setWebApplicationType(WebApplicationType.NONE);
-    // }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Thread
-            .currentThread()
-            .getContextClassLoader()
-            .getResource("vacations-ui.fxml"));
-        VBox page = (VBox) loader.load();
-        Scene scene = new Scene(page);
-
-        primaryStage.setTitle("Balance de vacaciones");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+@SpringBootApplication
+public class VacationsBalanceApplication {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(JavaFXApplication.class, args);
     }
 }
