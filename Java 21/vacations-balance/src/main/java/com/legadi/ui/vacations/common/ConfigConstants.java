60f74f5c0ec015594e614868d54ebaa081ab0d3f
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-11-28 18:26:49 -0600

    Configuration service

---
+++ Java 21/vacations-balance/src/main/java/com/legadi/ui/vacations/common/ConfigConstants.java
@@ -0,0 +1,22 @@
+package com.legadi.ui.vacations.common;
+
+public final class ConfigConstants {
+
+    private ConfigConstants() {}
+
+    public static final String FILE_TO_ANALYZE_LOCATION = "fileToAnalyzeLocation";
+    public static final String EMPLOYEE_NAME_FIRST_CELL = "employeeNameFirstCell";
+    public static final String TOTAL_TAKEN_DAYS_COLUMN = "totalTakenDaysColumn";
+    public static final String START_DATE_CELL = "startDateCell";
+    public static final String START_DATE_FORMAT = "startDateFormat";
+    public static final String BALANCE_DAYS_CELL = "balanceDaysCell";
+    public static final String PREVIOUS_VACATIONS_DAYS_CELL = "previousVacationsDaysCell";
+    public static final String RADIO_DAYS_CELL = "radioDaysCell";
+    public static final String ALLOWED_DAYS_YEAR_FIRST_CELL = "allowedDaysYearFirstCell";
+    public static final String ALLOWED_DAYS_VALUE_COLUMN = "allowedDaysValueColumn";
+    public static final String ALLOWED_DAYS_VALUE_ROW = "allowedDaysValueRow";
+    public static final String COMPANY_NAME_CELL = "companyNameCell";
+    public static final String BASE_YEAR = "baseYear";
+    public static final String TOTAL_DAYS_YEAR = "totalDaysYear";
+    public static final String TOTAL_DAYS_HALF_YEAR = "totalDyasHalfYear";
+}
---
+++ Java 21/vacations-balance/src/main/java/com/legadi/ui/vacations/service/ConfigService.java
@@ -0,0 +1,122 @@
+package com.legadi.ui.vacations.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ConfigService {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String DEFAULT_SETTINGS = "settings.json";
+
+    private final Logger logger = LoggerFactory.getLogger(ConfigService.class);
+
+    private final Map<String, Object> defaultProperties;
+    private final Map<String, Object> configProperties;
+    private final String configLocation;
+
+    public ConfigService(
+            @Value("${app.config.location}") String configLocation) {
+        this.defaultProperties = loadJsonInternal(DEFAULT_SETTINGS);
+        this.configProperties = loadInitialConfig(configLocation, this.defaultProperties);
+        this.configLocation = configLocation;
+    }
+
+    private Map<String, Object> loadInitialConfig(String configLocation, Map<String, Object> defaultProperties) {
+        File configFile = new File(configLocation);
+        if(configFile.exists()) {
+            return loadJsonFile(configLocation);
+        }
+
+        writeJsonFile(configLocation, defaultProperties);
+        return new HashMap<>(defaultProperties);
+    }
+
+    private Map<String, Object> loadJsonInternal(String location) {
+        try {
+            logger.info("Loading internal JSON file: {}", location);
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream jsonInputStream = classLoader.getResource(location).openStream();
+
+            try(Reader reader = new InputStreamReader(jsonInputStream)) {
+                return OBJECT_MAPPER.readValue(jsonInputStream, new TypeReference<>() {});
+            }
+        } catch(Exception ex) {
+            throw new IllegalStateException("Unable to obtain internal file: " + location, ex);
+        }
+    }
+
+    private Map<String, Object> loadJsonFile(String location) {
+        try(Reader reader = Files.newBufferedReader(Paths.get(location))) {
+            logger.info("Loading JSON file: {}", location);
+            return OBJECT_MAPPER.readValue(reader, new TypeReference<>() {});
+        } catch(Exception ex) {
+            throw new IllegalStateException("Unable to read JSON file: " + location, ex);
+        }
+    }
+
+    private void writeJsonFile(String location, Object content) {
+        try(Writer writer = Files.newBufferedWriter(getPathAndCreateDirs(location))) {
+            OBJECT_MAPPER.writeValue(writer, content);
+            logger.info("JSON file saved: {}", location);
+        } catch(IOException ex) {
+            throw new IllegalStateException("Unable to write JSON file: " + location, ex);
+        }
+    }
+
+    private Path getPathAndCreateDirs(String location) {
+        try {
+            Path filePath = Paths.get(location);
+            Files.createDirectories(filePath.getParent());
+            return filePath;
+        } catch(IOException ex) {
+            throw new IllegalStateException("Unable to create directories for: " + location);
+        }
+    }
+
+    public void override(String property, Object value) {
+        configProperties.put(property, value);
+        writeJsonFile(configLocation, value);
+    }
+
+    public String get(String property) {
+        return getValue(property, v -> v instanceof String);
+    }
+
+    public int getInt(String property) {
+        return getValue(property, v -> v instanceof Integer);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getValue(String propertyName, Predicate<Object> typeValidation) {
+        Object value = configProperties.getOrDefault(propertyName, defaultProperties.get(propertyName));
+        if(value == null) {
+            throw new IllegalStateException("Property value not found for: " + propertyName);
+        }
+        if(typeValidation.test(value)) {
+            return (T) value;
+        } else {
+            throw new IllegalStateException("Invalid value type ["
+                + (value != null ? value.getClass() : "null")
+                +  "]: " + value);
+        }
+    }
+}
