46c28b39353985f3f48d7d6fa89c6cf3adcb59e0
Giovanni Farfán B. <gfarfanb@gmail.com>
2026-02-16 16:39:12 -0600

    Missing tag

--- Java/Java 17/jdk17-features/src/test/java/com/legadi/jdk17/features/poc/PseudoRandomNumberGeneratorPocTest.java
+++ Java/Java 17/jdk17-features/src/test/java/com/legadi/jdk17/features/poc/PseudoRandomNumberGeneratorPocTest.java
@@ -17,11 +17,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@Tag("JEP-356")
 public class PseudoRandomNumberGeneratorPocTest {
 
     @Test
